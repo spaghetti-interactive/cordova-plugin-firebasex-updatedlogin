b94fba44f5d5c2ca8247401db39c637512835972
Franjanko <gtodini@gmail.com>
2022-10-04 16:40:41 +0200

    added user details on login ios

--- src/ios/FirebasePlugin.m
+++ src/ios/FirebasePlugin.m
@@ -772,10 +772,16 @@ static NSMutableDictionary* traces;
 
                     NSNumber* key = [[FirebasePlugin firebasePlugin] saveAuthCredential:credential];
                     NSString *idToken = user.authentication.idToken;
+                    NSString *userID = user.userID;
+                    NSString *emailAddress = user.profile.email;
+                    NSURL *imageUrl = [user.profile imageURLWithDimension:120]; // TODO pass in img size as param, and try to sync with Android
                     NSMutableDictionary* result = [[NSMutableDictionary alloc] init];
                     [result setValue:@"true" forKey:@"instantVerification"];
                     [result setValue:key forKey:@"id"];
                     [result setValue:idToken forKey:@"idToken"];
+                    [result setValue:emailAddress forKey:@"emailAddress"];
+                    [result setValue:userID forKey:@"userID"];
+                    [result setValue:imageUrl ? imageUrl.absoluteString : [NSNull null] forKey:@"imageUrl"];
                     pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:result];
                 } else {
                   pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:error.description];
